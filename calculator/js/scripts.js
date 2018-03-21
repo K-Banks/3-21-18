4682a2763f9a4f265a7227b0368f1985c76644e4
jared lutz and Kayl Eubanks <jared@>
2018-03-21 11:33:32 -0700

    add ability to subtract, mulitply, and divide, add bootstrap and jquery

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -1,8 +1,23 @@
-var add = function(number1, number2) {
-return number1 + number2;
-};
+$(document).ready(function(){
 
-var number1 = parseInt(prompt("why not enter a number?:"));
-var number2 = parseInt(prompt("enter another number:"));
+  var add = function(number1, number2) {
+  return number1 + number2;
+  };
 
-alert(add(number1,number2));
+  var subtract = function(number1, number2) {
+  return number1 - number2;
+  };
+
+  var multiply = function(number1, number2) {
+  return number1 * number2;
+  };
+
+  var divide = function(number1, number2) {
+  return number1 / number2;
+  };
+
+  var number1 = parseInt(prompt("why not enter a number?:"));
+  var number2 = parseInt(prompt("enter another number:"));
+
+  alert(add(number1,number2));
+});
